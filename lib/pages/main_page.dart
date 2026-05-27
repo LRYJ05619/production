22d1aa21ee05d2809c5e15a4e0ea5aeaa4006aa8
hmaya <1556984587@qq.com>
2026-05-27 17:18:07 +0800

    更新版本号

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -7,7 +7,7 @@ import '../widgets/app_toast.dart';
 import '../widgets/widgets.dart';
 import 'pages.dart';
 
-const String kAppVersion = 'v0.9.28';
+const String kAppVersion = 'v0.9.29';
 
 class MainPage extends StatefulWidget {
   final UserInfo userInfo;
